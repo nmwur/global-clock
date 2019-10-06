16d29230257a1da9d8efa5ab89ef0faceceac488
Konstantin <sunyris@gmail.com>
2019-10-06 14:14:13 +1300

    debug

--- client/src/ClockList/Clock.js
+++ client/src/ClockList/Clock.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { format, getMinutes, setMinutes, startOfMinute } from "date-fns";
+import { format } from "date-fns";
 import PropTypes from "prop-types";
 import * as d3 from "d3";
 
